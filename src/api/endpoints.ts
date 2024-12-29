73dcf35036cdb832298b794886eaa76ce6ed61c2
Samer <samerelmaz@gmail.com>
2024-12-29 16:13:52 -0400

    update fetcher env variable name

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3001/api";
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001/api";
 
 export const endpoints = {
   tasks: {
